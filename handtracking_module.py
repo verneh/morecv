6e3e9008c438b9e3c5b62ff87f92954eaafc33c3
Verne <verneh.com>
2021-07-18 12:41:32 +0200

    commit

--- handtracking_module.py
+++ handtracking_module.py
@@ -5,7 +5,6 @@ import time
 class handDetector():
 
     def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
-        
         self.mode = mode
         self.maxHands = maxHands
         self.detectionCon = detectionCon
@@ -14,8 +13,9 @@ class handDetector():
         self.hands = self.mpHands.Hands(self.mode, self.maxHands,
                                         self.detectionCon, self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
-    
+
     def findHands(self, img, draw=True):
+
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
         # print(results.multi_hand_landmarks)
@@ -25,7 +25,7 @@ class handDetector():
                     self.mpDraw.draw_landmarks(img, handLms,
                                                self.mpHands.HAND_CONNECTIONS)
         return img
-    
+
     def findPosition(self, img, handNo=0, draw=True):
         lmList = []
         if self.results.multi_hand_landmarks:
